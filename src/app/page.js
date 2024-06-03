23660d6822c0efda6bfea96e5c945308fd4b04f0
rhizal24 <rhizalrhoma@gmail.com>
2024-06-03 12:15:30 +0700

    nambah asu aja

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="aspect-[1280/720] w-full bg-[red]">
       <div className="flex justify-center items-center w-full h-full">
-        <div className="text-[90px] font-[900]">BERNARDS.</div>
+        <div className="text-[90px] font-[900]">BERNARDS ASU.</div>
       </div>
     </main>
   );
